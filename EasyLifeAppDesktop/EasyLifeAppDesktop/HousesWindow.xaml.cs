0982d12fc7728d492dfc67a9b0525d0ca26f3727
Iliyan Stoyanov <virus_22@abv.bg>
2019-01-29 19:50:15 +0200

    Created the project, th DB and the WPF Welcome Window.

---
+++ EasyLifeAppDesktop/EasyLifeAppDesktop/HousesWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasyLifeAppDesktop
+{
+    /// <summary>
+    /// Interaction logic for HousesWindow.xaml
+    /// </summary>
+    public partial class HousesWindow : Window
+    {
+        public HousesWindow()
+        {
+            InitializeComponent();
+            IEnumerable<House> houses = GetAllHouses();
+            
+            foreach(House house in houses)
+            {
+                housesList.Items.Add(house.Name);
+            }
+        }
+
+        private IEnumerable<House> GetAllHouses()
+        {
+            var el = new EasyLifeEntities();
+            var houses = el.Houses.ToList();
+            return houses;
+        }
+    }
+}
---
+++ EasyLifeAppDesktop/EasyLifeAppDesktop/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EasyLifeAppDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            welcomeLbl.Content += Environment.UserName;
+        }
+
+        private void HousesLbl_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var hw = new HousesWindow();
+            //this will open your child window
+            hw.Show();
+            //this will close parent window. windowOne in this case
+            Close();
+        }
+    }
+}
